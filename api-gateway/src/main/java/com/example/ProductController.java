d88f89a274828fa98384ba5aa77b6bde83f37eae
Dadaram Jadhav <dadaramjadhav@192.168.1.6>
2025-09-17 23:17:31 +0530

    updated

--- api-gateway/src/main/java/com/example/ProductController.java
+++ api-gateway/src/main/java/com/example/ProductController.java
@@ -11,18 +11,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1")
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/")
+    @GetMapping("/products")
     public List<Product> getEmployees() {
         return productRepository.findAll();
     }
 
-    @PostMapping("/")
+    @PostMapping("/products")
     public String addProduct(@RequestBody Product product) {
 
         return "product added..." + productRepository.save(product);
