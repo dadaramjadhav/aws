c01e5151844aa24a8dd25242cf386f5616d3e813
Dadaram Jadhav <dadaramjadhav@192.168.1.6>
2025-09-17 23:09:16 +0530

    api gateway app added

---
+++ api-gateway/src/main/java/com/example/Product.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+    @Id
+    private Integer id;
+    private String name;
+}
